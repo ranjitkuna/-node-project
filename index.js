1967fb962aca457716b264508d6b2d1592c0584d
RANJIT <ranjitrautaray123@gmail.com>
2023-03-01 21:48:11 +0530

    some new files added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const http  = require("http");
-
+const data = require("./data")
 http.createServer((req,res)=>{
-    res.write("i am working");
+    res.writeHead(200,{"Content-Type":"application/json"});
+    res.write(JSON.stringify(data))
     res.end()
 }).listen(5000)
 
