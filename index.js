0511c3788c9b5fdcb7281214bb0092080918969e
RANJIT <ranjitrautaray123@gmail.com>
2023-03-02 23:03:37 +0530

    new file added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
-const http  = require("http");
-const data = require("./data")
-http.createServer((req,res)=>{
-    res.writeHead(200,{"Content-Type":"application/json"});
-    res.write(JSON.stringify(data))
-    res.end()
-}).listen(5000)
 
-console.log("this is working");
+
+const input = process.argv;
+if(input[2]=="add"){
+    fs.writeFileSync(input[3],input[4]);
+}
+else if(input[2]=="remove"){
+    fs.unlinkSync(input[3])
+}
+else{
+    console.log("invalid input added")
+}
