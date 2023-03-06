805f0dbe971342989ec9d98ca84f5568ddca37d7
RANJIT <ranjitrautaray123@gmail.com>
2023-03-06 22:16:43 +0530

    some files added

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const path = require("path")
 
 const app = express();
 console.log(__dirname)
-
+let publickpath = path.join(__dirname,"files");
+// app.use(express.static(publickpath))
 app.get("",(req,res)=>{
-    res.send({
-        name:"ranjit",
-        age:34
-    })
+    res.sendFile(`${publickpath}/index.html`)
 })
-
+app.get("/all",(req,res)=>{
+    res.sendFile(`${publickpath}/about.html`)
+})
+app.get("*",(req,res)=>{
+    res.sendFile(`${publickpath}/404.html`)
+})
 app.listen(5400)
